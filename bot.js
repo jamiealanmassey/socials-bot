5a2915b30158d7c37c87268451fbee658a042a0c
Jamie Massey <jamiealanmassey@gmail.com>
2020-08-15 19:41:04 +0100

    added basic parsing functionality

--- bot.js
+++ bot.js
@@ -1,26 +1,49 @@
 // Grab libraries
-var Discord = require('discord.js')
-var logger = require('winston')
+const Discord = require('discord.js')
+const date = require('date-and-time')
 
 // Grab authorisation data and initialise discord.js
-const token = require('./token.json')
+const config = require('./config.json')
+const socials = require('./socials.json')
 const client = new Discord.Client();
 
-// Configure logger settings
-logger.remove(logger.transports.Console)
-logger.add(new logger.transports.Console, { colorize: true })
-logger.level = 'debug'
+function logify(message) {
+    const now = new Date()
+    const formatted = date.format(now, 'DD/MM/YY-HH:mm:ss')
+    return `[${formatted}] ${message}`
+}
 
 client.on('ready', () => {
-    logger.info('Connected')
-    logger.info('Logged in as: ')
-    logger.info(`${bot.username} - (${bot.id})`)
+    console.log(logify('Successfully connected to server'))
 })
 
-client.on('message', function(user, userID, channelID, message, evt) {
-    logger.info(`${bot.username} - user is ${user}`)
-    logger.info(`${bot.username} - userID is ${userID}`)
-    logger.info(`${bot.username} - channelID is ${channelID}`)
-    logger.info(`${bot.username} - message is [${message}]`)
-    logger.info(`${bot.username} - event is ${evt}`)
+client.on('message', message => {
+    if (message.member.hasPermission(Discord.Permissions.FLAGS.ADMINISTRATOR)) {
+        if (message.content.charAt(0) === '!') {
+            const parsed = message.content.split(' ')
+            if (parsed.length >= 2) {
+                if (parsed[0] === 'focus') {
+                    // TODO: set config to focus this channel instead
+                }
+            }
+        }
+        else {
+            var images = []
+            var imageExpr = /(http?s:\/\/)+(.*)\.(jpe?g|png|gif|bmp)+/gi
+            var result = message.content.replace(imageExpr, function(x) {
+                images.push(x)
+                return ''
+            })
+
+            console.log(logify('-- RECEIEVED MESSAGE --'))
+            console.log(logify(message.content.trim()))
+            console.log(logify('-- TRANSFORMED MESSAGE --'))
+            console.log(logify(result.trim()))
+            console.log(logify('-- FOUND IMAGE LINKS --'))
+            images.forEach(elem => console.log(logify(elem)))
+            console.log(logify(''))
+        }
+    }
 })
+
+client.login(config.bot_token)
